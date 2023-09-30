1fa357cf51bb8f6c0e043f2abb17a354febdaf0b
Raunak <raunakgiri21@gmail.com>
2023-09-30 08:43:01 +0530

    LinkedList Remove

--- Data_Structures/7-LinkedList.js
+++ Data_Structures/7-LinkedList.js
@@ -55,19 +55,60 @@ class LinkedList {
       console.log("Invalid index: " + index);
       return;
     }
-    if(index === 0){
+    if (index === 0) {
       this.prepend(value);
-    }
-    else {
+    } else {
       const node = new Node(value);
       let prev = this.head;
-      for(let i=0;i<index-1;i++){
+      for (let i = 0; i < index - 1; i++) {
         prev = prev.next;
       }
       node.next = prev.next;
       prev.next = node;
+      this.size++;
     }
-    this.size++;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+    let removedNode = this.head;
+    if (index === 0) {
+      this.head = this.head.next;
+    } else {
+      let prev = this.head;
+      while (index - 1) {
+        prev = prev.next;
+        index--;
+      }
+      removedNode = prev.next;
+      prev.next = removedNode.next;
+    }
+    this.size--;
+    return removedNode.value;
+  }
+
+  removeValue(value) {
+    if (this.size === 0) {
+      return null;
+    }
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      this.size--;
+      return value;
+    }
+    let prev = this.head;
+    while (prev.next.value !== value && prev.next !== null) {
+      prev = prev.next;
+    }
+    if (prev.next) {
+      let removedNode = prev.next;
+      prev.next = removedNode.next;
+      this.size--;
+      return value;
+    }
+    return null;
   }
 
   print() {
@@ -93,7 +134,10 @@ list.prepend(20);
 list.append(40);
 list.append(0);
 list.prepend(30);
-list.insert(50,0);
-list.insert(15,3);
+list.insert(50, 0);
+list.insert(15, 3);
+console.log(list.remove(3));
+console.log(list.remove(6));
+console.log(list.removeValue(50));
 list.print();
 console.log("Size: ", list.getSize());
